888ef70741198255f289663d18e0c7509ea161c8
Eloi Leitao <eloileitao.junior@gmail.com>
2020-07-07 22:17:54 +0100

    votes home page

--- app/src/main/java/com/example/projetotrinsheira/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/projetotrinsheira/RecyclerViewAdapter.java
@@ -19,6 +19,18 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.projetotrinsheira.ui.dashboard.DashboardViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.ByteArrayOutputStream;
 import java.time.LocalDate;
@@ -39,6 +51,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
    // private ArrayList<String> mPostsId= new ArrayList<>();
     private Context mContext;
     String imageStringPost;
+    private FirebaseAuth mAuth;
 
     public RecyclerViewAdapter(Context mContext, ArrayList<String> mImageNames, ArrayList<String> mImages, ArrayList<String> mImagePosts, ArrayList<String> mTime,ArrayList<String> mVotes, ArrayList<String> mDesc, ArrayList<String> mPostsId ) {
         this.mImageNames = mImageNames;
@@ -61,18 +74,24 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         Log.v("T", "onBindViewHolder: called.");
 
-        Glide.with(mContext)
+        /*Glide.with(mContext)
                 .asBitmap()
                 .load(StringToBitMap(mImages.get(position)))
                 .into(holder.image);
-        Glide.with(mContext)
+       /* Glide.with(mContext)
                 .asBitmap()
                 .load(StringToBitMap(mImagePosts.get(position)))
-                .into(holder.imagePost);
+                .into(holder.imagePost);*/
+
+        holder.imagePost.setImageBitmap(StringToBitMap(mImagePosts.get(position)));
+        holder.image.setImageBitmap(StringToBitMap(mImages.get(position)));
 
         holder.imageName.setText(mImageNames.get(position));
         holder.time.setText(mTime.get(position));
@@ -93,6 +112,239 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         });
 
+        final String postId= mPostsId.get(position);
+
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        final String userId = firebaseUser.getUid();
+
+        final String[] userIdField = new String[1];
+
+        db.collection("users")
+                .whereEqualTo("userId", userId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (final QueryDocumentSnapshot documentUser : task.getResult()) {
+                                // Log.v("Yes", "Entrou");
+
+                                Log.d("docUserVotes", "documentUser votes) data: " + documentUser.getString("votes"));
+                                String votesArray= documentUser.getString("votes");
+                                // userIdField[0] =documentUser.getString("userId");
+                                if(votesArray.equals("")){
+                                    Log.d("checkuserVotes", votesArray );
+                                    holder.votarBtn.setText("Votar");
+
+
+
+
+                                }
+                                else{
+                                    //votesArray.split()
+                                    String[] arrOfStr = votesArray.split(";", votesArray.length());
+                                    Log.d("checkuserVotes", "votesStringData" + arrOfStr[0]);
+                                    Boolean idPostFound=false;
+                                    for(int i=0;i<arrOfStr.length;i++){
+                                        if(arrOfStr[i].equals(postId)){
+                                            idPostFound=true;
+                                            i=arrOfStr.length;
+                                        }
+                                        else{
+                                            idPostFound=false;
+
+                                        }
+                                    }
+
+                                    if (idPostFound.equals(true)) {
+                                        holder.votarBtn.setText("Cancelar Voto");
+                                    }
+                                    else{
+                                        holder.votarBtn.setText("Votar");
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        holder.votarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+
+
+
+
+
+
+
+
+
+
+                final String[] stringVotes = new String[1];
+                final Integer[] numbVotes = new Integer[1];
+
+
+                //função para adicionar voto
+                DocumentReference docRef = db.collection("posts").document(postId);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            final DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d("postPage", "DocumentSnapshot data: " + document.getString("name"));
+
+                                /// get logged user votes
+
+
+                                numbVotes[0] =Integer.parseInt(document.getString("votes"));
+
+                                String btnTextString;
+
+                                if(holder.votarBtn.getText().equals("Votar")){
+                                    numbVotes[0]= numbVotes[0]+1;
+
+                                    db.collection("users")
+                                            .whereEqualTo("userId", userId)
+                                            .get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        for (final QueryDocumentSnapshot documentUser : task.getResult()) {
+                                                            // Log.v("Yes", "Entrou");
+
+                                                            String votesStringOld = documentUser.getString("votes");
+                                                            String votesStringNew = votesStringOld+";"+postId;
+
+                                                            DocumentReference userRef = db.collection("users").document(documentUser.getId());
+                                                            userRef.update("votes",votesStringNew);
+
+                                                            holder.votarBtn.setText("Cancelar Voto");
+
+
+                                                        }
+                                                    }
+                                                }
+                                            });
+
+                                }
+                                else{
+                                    numbVotes[0]= numbVotes[0]-1;
+                                    db.collection("users")
+                                            .whereEqualTo("userId", userId)
+                                            .get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        for (final QueryDocumentSnapshot documentUser : task.getResult()) {
+                                                            // Log.v("Yes", "Entrou");
+
+                                                            String votesStringOld = documentUser.getString("votes");
+                                                            String votesStringNew="";
+
+                                                            String[] arrOfStr = votesStringOld.split(";", votesStringOld.length());
+
+                                                            for(int i=0;i<arrOfStr.length;i++){
+                                                                if(arrOfStr[i].equals(postId)){
+
+                                                                }
+                                                                else{
+                                                                    votesStringNew=votesStringNew+";"+arrOfStr[i];
+                                                                }
+                                                            }
+
+                                                            DocumentReference userRef = db.collection("users").document(documentUser.getId());
+                                                            userRef.update("votes",votesStringNew);
+                                                            holder.votarBtn.setText("Votar");
+                                                        }
+                                                    }
+                                                }
+                                            });
+
+
+                                }
+
+
+
+                                DocumentReference docPostRef = db.collection("posts").document(postId);
+
+// Set the "isCapital" field of the city 'DC'
+
+                                docPostRef
+                                        .update("votes", numbVotes[0].toString())
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                holder.votes.setText(numbVotes[0].toString()+" votos");
+                                                Log.d("TAG", "DocumentSnapshot successfully updated!");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w("TAG", "Error updating document", e);
+                                            }
+                                        });
+
+
+
+
+
+
+
+
+
+                            } else {
+                                Log.d("postPage", "No such document");
+                            }
+                        } else {
+                            Log.d("postPage", "get failed with ", task.getException());
+                        }
+
+                        // Log.v("Yes", "Bro " + postComments);
+
+                    }
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        });
+
 
     }
 
@@ -103,6 +355,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+
         CircleImageView image;
         ImageView imagePost, imageUser;
         TextView imageName;
@@ -110,6 +363,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         TextView votes;
         TextView desc;
         RelativeLayout parentLayout;
+
+        Button votarBtn, comentarBtn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -133,6 +388,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             votes = itemView.findViewById(R.id.vote);
             desc = itemView.findViewById(R.id.desc);
             parentLayout = itemView.findViewById(R.id.parent_layout);
+            votarBtn= itemView.findViewById(R.id.votarBtnId);
+            comentarBtn= itemView.findViewById(R.id.comentarBtnId);
         }
     }
 
